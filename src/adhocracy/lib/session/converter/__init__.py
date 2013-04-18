647e3a7714ee5b1a08391354dd17e5ce76e8d61b
skoenen <stefan.koenen@uni-duesseldorf.de>
2013-04-18 14:50:44 +0200

    Collect all converter for more convienance

---
+++ src/adhocracy/lib/session/converter/__init__.py
@@ -0,0 +1,10 @@
+__all__ = [
+        "value_converter",
+        "plain_value_converter",
+        "signed_value_converter",
+        "crypt_alue_converter"]
+
+from value_converter import ValueConverter
+from plain_value_converter import PlainValueConverter
+from signed_value_converter import SignedValueConverter
+from crypt_value_converter import CryptValueConverter
