dacccddf762cac04e5a99efbfcd2160d8d1f7890
skoenen <stefan.koenen@uni-duesseldorf.de>
2013-04-19 13:32:06 +0200

    Added unicode testing to session test.

--- src/adhocracy/tests/lib/session/test_session.py
+++ src/adhocracy/tests/lib/session/test_session.py
@@ -7,6 +7,11 @@ from adhocracy.lib.session.converter import PlainValueConverter
 
 class SessionTestController(TestController):
     _domain = "adhocracy.lan"
+    _test_string = "\"'Perry' Rhodan\"☃⁋"
+    _test_encoded = "\\\"\\'Perry\\' Rhodan\\\"\x2603\x204b"
+    _test_cookie_value = "President={0}; Domain=.adhocracy.lan".
+            format(self._test_cookie_value)
+    _test_cookie_header = "Set-Cookie: {0}".format(self._test_cookie_value)
 
     def _config(self, **add_config):
         config = {}
@@ -19,22 +24,22 @@ class SessionTestController(TestController):
     def _environ(self, **add_environ):
         environ = {}
 
-        environ["HTTP_COOKIE"] = "Cookie: President=\"Perry Rhodan\";
-        Domain=.adhocray.lan"
+        environ["HTTP_COOKIE"] = "Cookie: {0}".format(self._test_cookie_value)
 
         environ.update(add_environ)
         return environ
 
-    def _check_with_session(self, session, *add_tests):
-        cookie_header = "Set-Cookie: President=\"Perry Rhodan\";
-        Domain=.adhocracy.lan"
-        session["President"] = "Perry Rhodan"
+    def session_with_no_extra_config(self):
+        session = Session({}, self._config(),
+                value_converter=PlainValueConverter)
+
+        session["President"] = self._test_string
 
         # Check that we get what we put into
-        self.assertEqual(session["President"], "Perry Rhodan")
+        self.assertEqual(session["President"], self._test_string)
 
         # Check that it produces the correct `Set-Cookie` header
-        self.assertEqual(session._cookie["President"], cookie_header)
+        self.assertEqual(session._cookie["President"], self._test_cookie_header)
 
         if "path_check" in add_tests:
             self.assertTrue("Path=/test" in session._cookie["President"])
@@ -42,28 +47,77 @@ class SessionTestController(TestController):
         if "http_only_check" in add_tests:
             self.assertTrue("HttpOnly" in session._cookie["President"])
 
-    def session_with_no_extra_config(self):
-        session = Session({}, self._config(),
-                value_converter=PlainValueConverter)
-        self._check_with_session(session)
-
     def session_with_path(self):
         session = Session({}, self._config(), path="/test",
                 value_converter=PlainValueConverter)
-        self._check_with_session(session, "path_check")
+
+        session["President"] = self._test_string
+
+        # Check that we get what we put into
+        self.assertEqual(session["President"], self._test_string)
+
+        # Check that it produces the correct `Set-Cookie` header
+        self.assertEqual(session._cookie["President"], self._test_cookie_header)
+
+        if "path_check" in add_tests:
+            self.assertTrue("Path=/test" in session._cookie["President"])
+
+        if "http_only_check" in add_tests:
+            self.assertTrue("HttpOnly" in session._cookie["President"])
 
     def session_with_http_only(self):
         session = Session({}, self._config(), http_only=True,
                 value_converter=PlainValueConverter)
-        self._check_with_session(session, "http_only_check")
+
+        session["President"] = self._test_string
+
+        # Check that we get what we put into
+        self.assertEqual(session["President"], self._test_string)
+
+        # Check that it produces the correct `Set-Cookie` header
+        self.assertEqual(session._cookie["President"], self._test_cookie_header)
+
+        if "path_check" in add_tests:
+            self.assertTrue("Path=/test" in session._cookie["President"])
+
+        if "http_only_check" in add_tests:
+            self.assertTrue("HttpOnly" in session._cookie["President"])
 
     def session_with_path_and_http_only(self):
         session = Session({}, self._config(), path="/test", http_only=True,
                 value_converter=PlainValueConverter)
-        self._check_with_session(session, "path_check", "http_only_check")
+
+        session["President"] = self._test_string
+
+        # Check that we get what we put into
+        self.assertEqual(session["President"], self._test_string)
+
+        # Check that it produces the correct `Set-Cookie` header
+        self.assertEqual(session._cookie["President"], self._test_cookie_header)
+
+        if "path_check" in add_tests:
+            self.assertTrue("Path=/test" in session._cookie["President"])
+
+        if "http_only_check" in add_tests:
+            self.assertTrue("HttpOnly" in session._cookie["President"])
+
 
     def session_load_from_cookie(self):
         session = Session(self._environ(), self._config(),
                 value_converter=PlainValueConverter)
-        self._check_with_session(session)
+
+        session["President"] = self._test_string
+
+        # Check that we get what we put into
+        self.assertEqual(session["President"], self._test_string)
+
+        # Check that it produces the correct `Set-Cookie` header
+        self.assertEqual(session._cookie["President"], self._test_cookie_header)
+
+        if "path_check" in add_tests:
+            self.assertTrue("Path=/test" in session._cookie["President"])
+
+        if "http_only_check" in add_tests:
+            self.assertTrue("HttpOnly" in session._cookie["President"])
+
 
