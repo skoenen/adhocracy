66d5ba64ce0dab914e8a29b0419a3cad6211e38f
skoenen <stefan.koenen@uni-duesseldorf.de>
2013-04-24 10:48:29 +0200

    Removed the CryptoValueConverter from adhocracy.lib.session.converter, so that this branch do not need pycrypto as dependency.

--- src/adhocracy/lib/session/converter/__init__.py
+++ src/adhocracy/lib/session/converter/__init__.py
@@ -1,10 +1,8 @@
 __all__ = [
         "value_converter",
         "plain_value_converter",
-        "signed_value_converter",
-        "crypt_alue_converter"]
+        "signed_value_converter"]
 
 from value_converter import ValueConverter
 from plain_value_converter import PlainValueConverter
 from signed_value_converter import SignedValueConverter
-from crypt_value_converter import CryptValueConverter
